786a1868964bb8d379358e60b36c49a0a03dd394
mariaspano <maria.spano@unina.it>
2025-12-29 09:28:29 +0100

    update iban

--- navbar-logos.js
+++ navbar-logos.js
@@ -10,11 +10,11 @@
       logosDiv.className = 'navbar-right-logos';
       logosDiv.style.cssText = 'display: flex; align-items: center; gap: 25px; margin-left: 20px;';
       
-      // Crea logo UNINA
-      const logoUnina = document.createElement('img');
-      logoUnina.src = 'images/logos/logo_unina.png';
-      logoUnina.alt = 'Università Federico II';
-      logoUnina.style.cssText = 'height: 50px; width: auto;';
+      // // Crea logo UNINA
+      // const logoUnina = document.createElement('img');
+      // logoUnina.src = 'images/logos/logo_unina.png';
+      // logoUnina.alt = 'Università Federico II';
+      // logoUnina.style.cssText = 'height: 50px; width: auto;';
       
       // Crea logo DiSES
       const logoDises = document.createElement('img');
